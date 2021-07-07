a0a32713f3060d932a1f0373f8800fe5c16697e1
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-07 09:39:01 -0400

    Intial Commit - Worlds of Sol Mod

---
+++ src/main/java/projectsol/worlds/of/sol/WorldsOfSol.java
@@ -0,0 +1,16 @@
+package projectsol.worlds.of.sol;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WorldsOfSol implements ModInitializer {
+
+
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Worlds of Sol Loading");
+
+
+
+	}
+}
---
+++ src/main/java/projectsol/worlds/of/sol/client/WorldsOfSolClient.java
@@ -0,0 +1,13 @@
+package projectsol.worlds.of.sol.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldsOfSolClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Worlds of Sol Loading");
+
+
+    }
+}
