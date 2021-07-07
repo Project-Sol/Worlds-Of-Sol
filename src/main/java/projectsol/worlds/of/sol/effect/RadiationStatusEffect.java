50e32be7a25490523c825b788d9c5c06b5915c74
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-07 17:06:01 -0400

    Added Exobone Armor and Radiation effect

---
+++ src/main/java/projectsol/worlds/of/sol/effect/RadiationStatusEffect.java
@@ -0,0 +1,36 @@
+package projectsol.worlds.of.sol.effect;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+import projectsol.worlds.of.sol.registry.ArmorRegistry;
+import projectsol.worlds.of.sol.registry.StatusEffectRegistry;
+
+public class RadiationStatusEffect extends StatusEffect {
+    public RadiationStatusEffect() {
+        super(
+                StatusEffectType.HARMFUL,
+                0xA9A01C);
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        if (entity.getEquippedStack(EquipmentSlot.CHEST).isOf(ArmorRegistry.EXOBONE_CHESTPLATE)
+                && entity.getEquippedStack(EquipmentSlot.HEAD).isOf(ArmorRegistry.EXOBONE_HELMET)
+                && entity.getEquippedStack(EquipmentSlot.LEGS).isOf(ArmorRegistry.EXOBONE_LEGGINGS)
+                && entity.getEquippedStack(EquipmentSlot.FEET).isOf(ArmorRegistry.EXOBONE_BOOTS))
+        {
+            entity.removeStatusEffect(StatusEffectRegistry.RADIATION);
+        } else {
+            entity.damage(DamageSource.MAGIC, 1.0F * 1 + amplifier);
+        }
+    }
+}
