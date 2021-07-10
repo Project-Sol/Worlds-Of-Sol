7831cd5239c53420ea9eded10cc29b1c19776f86
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-10 17:17:15 -0400

    New Dimension - Flat Gen no surface builder

--- src/main/java/projectsol/worlds/of/sol/world/dimension/MoonChunkGenerator.java
+++ src/main/java/projectsol/worlds/of/sol/world/dimension/MoonChunkGenerator.java
@@ -3,18 +3,22 @@ package projectsol.worlds.of.sol.world.dimension;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.chunk.StructuresConfig;
-import net.minecraft.world.gen.chunk.VerticalBlockSample;
+import net.minecraft.world.gen.chunk.*;
+import projectsol.worlds.of.sol.registry.BlockRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+
 
 public class MoonChunkGenerator extends ChunkGenerator {
 
@@ -51,12 +55,42 @@ public class MoonChunkGenerator extends ChunkGenerator {
 
     @Override
     public CompletableFuture<Chunk> populateNoise(Executor executor, StructureAccessor accessor, Chunk chunk) {
+        List<BlockState> list = new ArrayList<BlockState>();
+        list.add(Blocks.BEDROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        list.add(BlockRegistry.MOON_ROCK.getDefaultState());
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        Heightmap heightmap = chunk.getHeightmap(Heightmap.Type.OCEAN_FLOOR_WG);
+        Heightmap heightmap2 = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE_WG);
+
+        for(int i = 0; i < Math.min(chunk.getHeight(), list.size()); ++i) {
+            BlockState blockState = (BlockState)list.get(i);
+            if (blockState != null) {
+                int j = chunk.getBottomY() + i;
+
+                for(int k = 0; k < 16; ++k) {
+                    for(int l = 0; l < 16; ++l) {
+                        chunk.setBlockState(mutable.set(k, j, l), blockState, false);
+                        heightmap.trackUpdate(k, j, l, blockState);
+                        heightmap2.trackUpdate(k, j, l, blockState);
+                    }
+                }
+            }
+        }
         return CompletableFuture.completedFuture(chunk);
     }
 
     @Override
     public int getHeight(int x, int z, Heightmap.Type heightmapType, HeightLimitView heightLimitView) {
-        return 0;
+        return 70;
     }
 
     @Override
