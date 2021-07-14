ab065723e6c26f4c6e01aa12a37a24d439b33ef3
mrsterner <simon@sterner.dev>
2021-07-14 18:46:21 +0200

    Added Biome, Comet tundra and Lunar highground

--- src/main/java/projectsol/worldsofsol/common/statuseffect/RadiationStatusEffect.java
+++ src/main/java/projectsol/worldsofsol/common/statuseffect/RadiationStatusEffect.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
+import projectsol.worldsofsol.common.registry.SolObjects;
 import projectsol.worldsofsol.common.registry.SolStatusEffects;
 
 public class RadiationStatusEffect extends StatusEffect {
@@ -22,10 +23,10 @@ public class RadiationStatusEffect extends StatusEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity.getEquippedStack(EquipmentSlot.CHEST).isOf(ArmorRegistry.EXOBONE_CHESTPLATE)
-                && entity.getEquippedStack(EquipmentSlot.HEAD).isOf(ArmorRegistry.EXOBONE_HELMET)
-                && entity.getEquippedStack(EquipmentSlot.LEGS).isOf(ArmorRegistry.EXOBONE_LEGGINGS)
-                && entity.getEquippedStack(EquipmentSlot.FEET).isOf(ArmorRegistry.EXOBONE_BOOTS))
+        if (entity.getEquippedStack(EquipmentSlot.CHEST).isOf(SolObjects.EXOBONE_CHESTPLATE)
+                && entity.getEquippedStack(EquipmentSlot.HEAD).isOf(SolObjects.EXOBONE_HELMET)
+                && entity.getEquippedStack(EquipmentSlot.LEGS).isOf(SolObjects.EXOBONE_LEGGINGS)
+                && entity.getEquippedStack(EquipmentSlot.FEET).isOf(SolObjects.EXOBONE_BOOTS))
         {
             entity.removeStatusEffect(SolStatusEffects.RADIATION);
         } else {
