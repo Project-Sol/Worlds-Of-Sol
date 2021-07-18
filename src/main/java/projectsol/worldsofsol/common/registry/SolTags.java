4a2d4c51fd6acf63e7104fd66604f19d51bab6ff
mrsterner <simon@sterner.dev>
2021-07-18 16:23:51 +0200

    Tag based rad protection

--- src/main/java/projectsol/worldsofsol/common/registry/SolTags.java
+++ src/main/java/projectsol/worldsofsol/common/registry/SolTags.java
@@ -2,10 +2,12 @@ package projectsol.worldsofsol.common.registry;
 
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 import projectsol.worldsofsol.WorldsOfSol;
 
 public class SolTags {
     public static final Tag<EntityType<?>> SPACE_ENTITY = TagRegistry.entityType(new Identifier(WorldsOfSol.MODID, "space_type"));
+    public static final Tag<Item> RAD_SUITE = TagRegistry.item(new Identifier(WorldsOfSol.MODID, "rad_suite"));
 }
