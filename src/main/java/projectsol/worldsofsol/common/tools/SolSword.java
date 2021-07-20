2aa7cde26c9b87a400b40b1c59f01df03a3a087e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-20 10:59:58 -0400

    Updated BasicSwordItem -> SolSword

---
+++ src/main/java/projectsol/worldsofsol/common/tools/SolSword.java
@@ -0,0 +1,11 @@
+package projectsol.worldsofsol.common.tools;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class SolSword extends SwordItem {
+
+    public SolSword(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
